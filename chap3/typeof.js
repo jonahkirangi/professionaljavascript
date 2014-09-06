cf8fa39f461f773d5b73bc3ff2ed6da88d92df8c
Jonah Kirangi <jonahkirangi@gmail.com>
2014-09-05 17:20:26 -0700

    typeof operator

---
+++ chap3/typeof.js
@@ -0,0 +1,28 @@
+var datatype1 = "Jack and Jill went up the hill";
+var datatype2 = "To fetch a pail of water.";
+var datatype3 = "Jack fell down and broke his crown,";
+var datatype4 = "And Jill came tumbling after.";
+var datatype5 = true;
+var datatype6 = 95;
+var datatype7 = false;
+var datatype8 = {
+  driver: "Jessica",
+  shotgun: "John",
+  backseat1: "Steve",
+  backseat2: "Nicole",
+  backseat3: "Bob"};
+var datatype9 = function() {
+  return "Make cool things";
+};
+var datatype10;
+
+console.log(typeof(datatype1));
+console.log(typeof(datatype2));
+console.log(typeof(datatype3));
+console.log(typeof(datatype4));
+console.log(typeof(datatype5));
+console.log(typeof(datatype6));
+console.log(typeof(datatype7));
+console.log(typeof(datatype8));
+console.log(typeof(datatype9));
+console.log(typeof(datatype10));
