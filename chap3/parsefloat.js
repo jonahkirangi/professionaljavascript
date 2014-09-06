f1bc8925c7786f910bfb7d35526da3fd24be3f7b
Jonah Kirangi <jonahkirangi@gmail.com>
2014-09-06 16:42:44 -0700

    parseFloat()

---
+++ chap3/parsefloat.js
@@ -0,0 +1,10 @@
+console.log(parseFloat("Hello World"));
+console.log(parseFloat("1234 Hello World"));
+console.log(parseFloat("Hello World 1234"));
+
+console.log(parseFloat("123"));
+console.log(parseFloat("123.321"));
+console.log(parseFloat("123.321.123"));
+
+console.log(parseFloat(123));
+console.log(parseFloat(123.321));
