c2ee04c1a169503c109323f7cb7c6fda3046fa62
Jonah Kirangi <jonahkirangi@gmail.com>
2014-09-09 00:22:36 -0700

    Explore he unary plus and minus operators

---
+++ chap3/unaryminus.js
@@ -0,0 +1,25 @@
+var s1 = "01";
+var s2 = "1.1";
+var s3 = "z";
+var b = false;
+var f = 1.1;
+var o = {
+  valueOf: function() {
+    return -1;
+  }
+};
+
+// Unary Minus
+s1 = -s1;
+s2 = -s2;
+s3 = -s3;
+b = -b;
+f = -f;
+o = -o;
+
+console.log(s1);
+console.log(s2);
+console.log(s3);
+console.log(b);
+console.log(f);
+console.log(o);
