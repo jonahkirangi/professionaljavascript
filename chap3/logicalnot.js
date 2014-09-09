f6e8f9dd83d9192a79c15e5eaaee64a038f5872a
Jonah Kirangi <jonahkirangi@gmail.com>
2014-09-09 00:40:59 -0700

    Explore logical not

---
+++ chap3/logicalnot.js
@@ -0,0 +1,13 @@
+var logical1 = false;
+var logical2 = "blue";
+var logical3 = 0;
+var logical4 = NaN;
+var logical5 = "";
+var logical6 = 12345;
+
+console.log(!logical1);               // True
+console.log(!logical2);               // False
+console.log(!logical3);               // True
+console.log(!logical4);               // True
+console.log(!logical5);               // True
+console.log(!logical6);               // False
