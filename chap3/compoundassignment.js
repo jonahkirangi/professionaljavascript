eeed24b20f9fc664248ab59ef74b6afa9412351a
Jonah Kirangi <jonahkirangi@gmail.com>
2014-09-10 10:32:45 -0700

    Review compound assignment operators

---
+++ chap3/compoundassignment.js
@@ -0,0 +1,24 @@
+// Add & assign
+var num1 = 10;
+num1 += 10;
+console.log("10 + 10 = " + num1);
+
+// Divide & assign
+var num2 = 100;
+num2 /= 10;
+console.log("100 / 10 = " + num2);
+
+// Modulus & assign
+var num3 = 50;
+num3 %= 4;
+console.log("50 % 4 = " + num3);
+
+// Multiply & assign
+var num4 = 25;
+num4 *= 4;
+console.log("25 * 4 = " + num4);
+
+// Subtract & assign
+var num5 = 75;
+num5 -= 25;
+console.log("75 - 25 = " + num5);
