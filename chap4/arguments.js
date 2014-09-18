76c27c72ca6c7a49b5ea20bf1f7d8369250e44bc
Jonah Kirangi <jonahkirangi@gmail.com>
2014-09-17 23:07:11 -0700

    Passing arguments with primative and reference values

---
+++ chap4/arguments.js
@@ -0,0 +1,10 @@
+function addTen(num) {
+  num += 10;
+  return num;
+}
+
+var count = 20;
+console.log(count);                   // The variable count is written to the console
+
+var result = addTen(count);
+console.log(result);                  // The function is called and the variable count is passed
