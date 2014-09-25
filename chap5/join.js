9cf6491e855a0cc43eadb1b8b5ff093180fd40ad
Jonah Kirangi <jonahkirangi@gmail.com>
2014-09-24 19:28:52 -0700

    Create strings with the join() method

---
+++ chap5/join.js
@@ -0,0 +1,12 @@
+// Create new array
+var weather = ["Sunny", "Rainy", "Cloudy", "Drought"];
+
+// Create a string separated by a comma using join() method
+console.log(weather.join(", "));
+
+// Create a string separated by double pipes using join() method
+console.log(weather.join(" || "));
+
+// Create a string using join() method and save it to a variable
+var newString = weather.join(" - ");
+console.log(newString);
