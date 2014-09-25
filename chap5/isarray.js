5b3c7ac5e7762b05581172d41be53dabf3ca87d7
Jonah Kirangi <jonahkirangi@gmail.com>
2014-09-24 19:11:29 -0700

    Use the Array.isArray() method

---
+++ chap5/isarray.js
@@ -0,0 +1,13 @@
+// Create new array
+var cities = ["Seattle", "Boston", "Phoenix", "Houston"];
+
+// Use isArray mathod to confirm if cities is an array
+if (Array.isArray(cities)) {
+  // Print the third position of cities array
+  console.log(cities[3]);
+}
+
+if (Array.isArray(cities)) {
+  // Print entire cities array
+  console.log(cities);
+}
