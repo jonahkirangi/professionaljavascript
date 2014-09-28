02d6ae2edef839a85abc74c59b22449252926c49
Jonah Kirangi <jonahkirangi@gmail.com>
2014-09-27 20:58:37 -0700

    Exploring the slice() method

---
+++ chap5/slice.js
@@ -0,0 +1,8 @@
+var veggies = ["spinach", "kale", "green bean", "cabbage", "broccoli", "carrot", "cucumber"];
+
+var someVeggies = veggies.slice(3);
+var moreVeggies = veggies.slice(2,5);
+
+console.log(veggies);
+console.log(someVeggies);
+console.log(moreVeggies);
