55463b498652ee1da3c43a8816eddcea080b1f62
Jonah Kirangi <jonahkirangi@gmail.com>
2014-09-27 20:49:13 -0700

    Exploring the concat() method

---
+++ chap5/concat.js
@@ -0,0 +1,9 @@
+// Create a new array
+var animals1 = ["dog", "cat", "pig"];
+
+// Use concat() method to add to the first array
+// Individual items, as well as new arrays can be successfully added
+var animals2 = animals1.concat("horse", "eagle", ["gorilla", "fish"]);
+
+console.log(animals1);
+console.log(animals2);
