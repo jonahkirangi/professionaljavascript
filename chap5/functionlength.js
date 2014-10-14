fdd0ccb9391d3b9957da42e8eacb2c05eea89efe
Jonah Kirangi <jonahkirangi@gmail.com>
2014-10-13 19:47:27 -0700

    Function length property

---
+++ chap5/functionlength.js
@@ -0,0 +1,15 @@
+function sayName(name) {
+  return name;
+}
+
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+function sayHi() {
+  return "Hi";
+}
+
+console.log(sayName.length);
+console.log(sum.length);
+console.log(sayHi.length);
